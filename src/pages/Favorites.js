97983b7bf2a5993e8e7fdafd06acf7f907197fc8
Vishal Kumar <VKumar61@slb.com>
2022-01-24 18:50:00 +0530

    updates on useContext

--- src/pages/Favorites.js
+++ src/pages/Favorites.js
@@ -1,5 +1,21 @@
+import { useContext } from 'react';
+import FavoritesContext from '../store/favorites-context';
+import MeetupList from '../components/meetups/MeetupList';
+
 function Favorites() {
-    return <div>All Favorites Page</div>
+    const favoritesCtx = useContext(FavoritesContext);
+    let content;
+
+    if (favoritesCtx.totalFavorities === 0 ) {
+        content = <p>You got no favorites yet. Start adding some</p>
+    } else {
+        content = <MeetupList meetups={favoritesCtx.favorites} ></MeetupList>
+    }
+
+    return <section>
+        <h1>My Favorite</h1>
+        {content}
+    </section>
 }
 
 export default Favorites;
