8e04fdfb8df9518dc168e3fd2369460b932cab2a
Vishal Kumar <VKumar61@slb.com>
2022-01-24 17:53:08 +0530

    use effect is added

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetup() {
-    return <div>All NewMeetup Page</div>
+    const navigate = useNavigate();
+    function addMeetupHandler(meetupData) {
+        fetch(
+            'https://react-meetup-app-b7e5c-default-rtdb.firebaseio.com/meetups.json',
+            {
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: {
+                    'Content-Type': 'application/json'
+                }
+            }
+        ).then(() => {
+            navigate('/');
+        });
+    }
+    return <section>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm onAddMeetup={addMeetupHandler} ></NewMeetupForm>
+    </section>
 }
 
 export default NewMeetup;
